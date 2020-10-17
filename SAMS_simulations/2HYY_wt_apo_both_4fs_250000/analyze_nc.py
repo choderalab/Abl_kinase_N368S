ff30ffcd205f924ef8bace84e7e8a163f9f7bd50
Jiaye Guo <jiaye.guo@choderalab.org>
2020-10-16 20:40:20 -0400

    adding file for simulations

---
+++ SAMS_simulations/2HYY_wt_apo_both_4fs_250000/analyze_nc.py
@@ -0,0 +1,26 @@
+from netCDF4 import Dataset
+import mdtraj as md
+import numpy as np
+
+def print_traj():
+    experiment = 'both'
+    pdbid = '2HYY'
+    iteration = 250000
+    # get some info directly from nc file
+    ncfile = Dataset('traj_checkpoint.nc', 'r')
+
+    # Get an MDTraj topology from a PDB file that matches the atom ordering in the saved trajectories
+    print("Generating trajectory ...")
+    mdtraj_topology = md.load(f'{pdbid}_chainA_apo_minequi.pdb').topology
+    # Now get the positions from the trajectory
+    replica_index = 0
+    positions = ncfile.variables['positions'][:,replica_index,:,:] # gets all frames from the first replica
+    # Now create an MDTraj trajectory
+    mdtraj_trajectory = md.Trajectory(positions, mdtraj_topology)
+    # Write the trajectory in gromacs XTC format
+    outfilename = f'{experiment}_{iteration}_traj.dcd'
+    mdtraj_trajectory.save(outfilename)
+    return
+
+# print traj
+print_traj()
